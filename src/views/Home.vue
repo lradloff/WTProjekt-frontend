599d855c050521c38738a591e00b09258b37526c
lradloff <lennart-radloff-studium@outlook.de>
2021-11-24 17:20:20 +0100

    Test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo2.jpg">
     <HelloWorld msg="deine olle fette mutter stintk du hundesohnn  hahahahaaha"/>
+    <img alt="stfu" src="https://i.redd.it/jkikpfmulaj71.jpg">
+    <video width="320" height="240" controls>
+      <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
   </div>
 </template>
 
