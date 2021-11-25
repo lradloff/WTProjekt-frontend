2a3b78efec8e4ae140639acc5dfddacd92192c86
lradloff <lennart-radloff-studium@outlook.de>
2021-11-25 16:39:46 +0100

    Bootstrap Navbar

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 </template>
 
 <script>
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
-  components: { Navbar }
-}
+  components: { Navbar },
+};
 </script>
 
 <style>
