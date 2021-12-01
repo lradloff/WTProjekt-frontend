8172b9d4464e28db8cf104166272617f4b3993e9
lradloff <lennart-radloff-studium@outlook.de>
2021-12-01 14:20:18 +0100

    Icon changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="logo" src="src/assets/calclogo.png">
+    <img alt="logo" src="src/assets/logo.png">
     <HelloWorld msg="Hello World!"/>
   </div>
 </template>
